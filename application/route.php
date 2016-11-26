f832f8706d14c765116fe3a01fb433b626176de3
long250561603 <250561603@qq.ocm>
2016-11-26 15:05:44 +0800

    更新到TP5.03

--- application/route.php
+++ application/route.php
@@ -13,5 +13,9 @@ return [
     '__pattern__' => [
         'name' => '\w+',
     ],
+    '[hello]'     => [
+        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
+        ':name' => ['index/hello', ['method' => 'post']],
+    ],
 
 ];
--- build.php
+++ build.php
@@ -14,7 +14,7 @@ return [
     '__file__' => ['common.php', 'config.php', 'database.php'],
 
     // 定义demo模块的自动生成 （按照实际定义的文件名生成）
-    'home'     => [
+    'demo'     => [
         '__file__'   => ['common.php'],
         '__dir__'    => ['behavior', 'controller', 'model', 'view'],
         'controller' => ['Index', 'Test', 'UserType'],
@@ -22,11 +22,4 @@ return [
         'view'       => ['index/index'],
     ],
     // 其他更多的模块定义
-    'admin'     => [
-        '__file__'   => ['common.php'],
-        '__dir__'    => ['behavior', 'controller', 'model', 'view'],
-        'controller' => ['Index', 'Test', 'UserType'],
-        'model'      => ['User', 'UserType'],
-        'view'       => ['index/index'],
-    ],
 ];
