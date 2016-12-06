56dbf365180f32f88a3922852a01b85f7fc2ef60
long250561603 <250561603@qq.ocm>
2016-12-06 15:12:09 +0800

    商品模块

--- application/admin/controller/Goods.php
+++ application/admin/controller/Goods.php
@@ -12,9 +12,23 @@ namespace app\admin\controller;
 class Goods extends Base
 {
     public function lst(){
+        $goodsData = db('goods')->select();
+        $this->assign('goodsData',$goodsData);
         return $this->fetch();
     }
     public function add(){
+        if(request()->isPost()) {
+            $data            = $this->request->post();
+                $res = model('goods')->allowField(true)->save($data);
+                if ($res) {
+                    $this->success('操作成功', url('lst'));
+                } else {
+                    $this->error('操作失败');
+                }
+        }
+        return $this->fetch();
+    }
+    public function edt(){
         return $this->fetch();
     }
 }
---
+++ application/admin/model/Goods.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PC
+ * Date: 2016/12/6
+ * Time: 14:41
+ */
+
+namespace app\admin\model;
+
+use think\Model;
+class Goods extends Model
+{
+
+}
